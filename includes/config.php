d395ea86a27fc990fea2872b3d2dc441e359e424
Nabil Farhan <34857007+nabilfarhann@users.noreply.github.com>
2019-10-20 06:08:38 +0800

    Remove heroku host, username and password

--- includes/config.php
+++ includes/config.php
@@ -6,7 +6,7 @@ session_start();
 date_default_timezone_set("Asia/Kuala_Lumpur");
 
 try{
-	$con = new PDO("mysql:dbname=heroku_4f44aeeb4439d12;host=us-cdbr-iron-east-02.cleardb.net", "bdfe6f5cb6b764", "975b0a1f");
+	$con = new PDO("**Your Host Name**", "**Your DB name**", "**Your DB Password**");
 	$con-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 } catch(PDOException $e){
 	echo "Connection failed: " . $e->getMessage();
