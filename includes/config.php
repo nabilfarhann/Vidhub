827afcb666ab8cad877acc58e57f7354ea908528
Nabil Farhan <nnabilfarhan@gmail.com>
2021-06-25 16:41:30 +0800

    Change PDO connection for localhost

--- includes/config.php
+++ includes/config.php
@@ -6,7 +6,8 @@ session_start();
 date_default_timezone_set("Asia/Kuala_Lumpur");
 
 try{
-	$con = new PDO("**Your Host Name**", "**Your DB name**", "**Your DB Password**");
+	// $con = new PDO("**Your Host Name**", "**Your DB name**", "**Your DB Password**");
+	$con = new PDO("mysql:host=localhost;dbname=vidhub", 'root', 'root');
 	$con-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 } catch(PDOException $e){
 	echo "Connection failed: " . $e->getMessage();
